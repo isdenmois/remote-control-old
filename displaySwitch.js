ed1cb77deb5c0506f79d3706fd3a51196c643eb0
Denis Moiseev <isdenmois@gmail.com>
2017-03-08 20:28:34 +0000

    Fix path to DisplaySwitch

--- displaySwitch.js
+++ displaySwitch.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 module.exports = function (type) {
     const args = [ `/${type}` ];
-    const prog = path.join(process.env.SystemRoot, 'DisplaySwitch', '.exe');
+    const prog = path.join(process.env.SystemRoot, 'System32', 'DisplaySwitch.exe');
     return spawn(prog, args, {});
 };
