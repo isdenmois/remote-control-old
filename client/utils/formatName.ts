cd42fe12b7636023beedb97d01b68e4559cdcea1
Denis Moiseev <isdenmois@gmail.com>
2017-07-25 22:41:56 +0300

    Update filters

--- client/utils/formatName.ts
+++ client/utils/formatName.ts
@@ -2,9 +2,10 @@
 export default function (name: string, parentName: string = '') {
     const result = parentName.length > 0 ? name.replace(parentName, '') : name
     return result
-        .replace(/(\.ru\.|\.en\.|\.eng\.|\.rus\.)/gi, '.')
-        .replace(/(1080p|720p)/gi, '')
-        .replace(/(mkv|avi)/gi, '')
-        .replace(/(webrip|x264|5\.1|dd|HDTVRip|BDRip|WEB-DLRip|XviD)/gi, '')
+        .replace(/(3xRus|ru|en|eng|rus)\./gi, '.')
+        .replace(/(1080p|720p|480p|1080i|pk|1024x)/gi, '')
+        .replace(/(mkv|avi|ts|m4v|m4b)$/gi, '')
+        .replace(/(webrip|x264|5\.1|dd|HDTVRip|BDRip|WEB-DLRip|XviD|HDTV|WEB-DL|BluRay|BD|SD)/gi, '')
+        .replace(/(GeneralFilm|HDCLUB|Кравец|Doc66|\(\)|\[])/gi, '')
         .replace(/([_.\/\\])/g, ' ')
 }
